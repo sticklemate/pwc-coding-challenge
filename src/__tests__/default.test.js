57b6e945064ece2143d33adde103f004191a2f36
sticklemate <sticklemate@gmail.com>
2021-02-04 22:51:19 +1100

    Add default unit test for components

---
+++ src/__tests__/default.test.js
@@ -0,0 +1,11 @@
+
+/* TDD - Components testing */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('MyComponent', () => {
+  it('should render correctly in "debug" mode', () => {
+    // add unit test cases
+  });
+});
